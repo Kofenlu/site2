298cd31283255a59bba95ec89e915adf5e740877
radiance <nik.onetap@mail.ru>
2024-02-01 00:40:07 +0300

    initial

--- kofenlu_site/asgi.py
+++ kofenlu_site/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for boboev_site project.
+ASGI config for kofenlu_site project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boboev_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kofenlu_site.settings')
 
 application = get_asgi_application()
